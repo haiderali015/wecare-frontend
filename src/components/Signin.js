4d30c937b37cea054b571e2ab1fad02208195d50
haiderali015 <ha347278@gmail.com>
2023-02-16 15:40:24 +0500

    sigin done by haider

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -3,20 +3,46 @@ import { Link } from 'react-router-dom'
 import Navbar from './navbar';
 import Footer from './Footer';
 import Axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+    const navigate = useNavigate();
+
     const [username,setUsername]=useState("")
     const [password,setPassword]=useState("")
 
-    const login =()=>{
-        Axios.post("http://localhost:8001/login", {
+    const login = async  (event) => {
+            event.preventDefault(); // prevent default form submission behavior
+            console.log(event);
+          
+        // console.log(username);
+        // console.log(password);
+
+        const userData=await Axios.post("http://localhost:8001/login", {
+            
             username:username,
             password:password,
-    }).then ((response)=>{
-        console.log(response);
+            
+            
     })
+    // console.log(userData.data[0].id)
+    if(userData.data[0]){
+    if(userData.data[0].name==username)
+    {
+        console.log("redirect")
+        navigate('/Pharmacy');
     }
+    else{
+        console.log("wrong id ")
+    }
+}
+else{
+
+    console.log("error")
+}
+    
+    
+  }
 
   return (
         <><Navbar/>
@@ -28,8 +54,9 @@ const Signin = () => {
                 <div className="account-wall">
                     <img className="profile-img" src="https://www.citypng.com/public/uploads/preview/-115891627739whel2jtx7.png"
                         alt=""/>
-                    <form className="form-signin" action='/' method='POST'>
-                    <input type="text" className="form-control" placeholder="name" style={{textTransform:"lowercase" }} onChange={(e)=>{setUsername(e.target.value)}} autofocus required/>
+                    <form className="form-signin" action='' method='POST'>
+
+                    <input type="text" className="form-control" placeholder="name" style={{textTransform:"lowercase" }} onChange={(e)=>{setUsername(e.target.value)}} autoFocus required/>
                     <input type="password" className="form-control" placeholder="Password" style={{textTransform:"lowercase" }} onChange={(e)=>{setPassword(e.target.value)}} required/>
                     <button className="btn btn-lg btn-primary btn-block" value="LOGIN" onClick={login}>
                         Sign in</button>
